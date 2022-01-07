593d900a1b6ee6b1fd3a43789de9180053393868
江荣朝 <rcjiang@163.com>
2022-01-07 22:35:40 +0800

    update Catalog

--- src/component/catalog/Catalog.js
+++ src/component/catalog/Catalog.js
@@ -1,6 +1,29 @@
-function Catalog () {
+import * as React from 'react'
+import { AppBar, Box, Tabs, Tab } from '@mui/material'
+
+function Catalog() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event, newValue) => {
+    console.log(newValue)
+    setValue(newValue)
+  }
+
   return (
-    <div>catalog</div>
+    <Box sx={{ width: '100%' }}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          textColor="inherit"
+          indicatorColor="secondary"
+        >
+          <Tab label="作者" value="1" />
+          <Tab label="年代" value="2" />
+          <Tab label="体裁" value="3" />
+        </Tabs>
+      </AppBar>
+    </Box>
   )
 }
 
