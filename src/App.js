7085c3ba330036666fbb5dd562f0499a5be14830
江荣朝 <rcjiang@163.com>
2022-01-10 22:23:35 +0800

    use useRoutes replace <Routes/> and  lazy loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import { Routes, Route, Navigate } from "react-router-dom"
 import './App.css'
 import RouteTabs from './component/nav/RouteTabs'
-import ClassMgt from './component/class/ClassMgt'
-import ContentMgt from './component/content/ContentMgt'
-import Fields from "./component/fields/Fields"
-import Field from "./component/fields/Field"
-import Contents from './component/content/Contents'
-import Content from './component/content/Content'
+import Routes from './route';
 
 function App() {
   return (
@@ -15,19 +9,7 @@ function App() {
         <RouteTabs />
       </nav>
       <main>
-        <Routes>
-          <Route index element={<Navigate to="/entity" replace={true} />} />
-          <Route path="/entity" element={<ClassMgt />} >
-            <Route path=":entityId">
-              <Route index element={<Fields />} />
-              <Route path=":fieldId" element={<Field />} />
-            </Route>
-          </Route>
-          <Route path="/contents" element={<ContentMgt />} >
-            <Route index element={<Contents />} />
-            <Route path="detail" element={<Content />} />
-          </Route>
-        </Routes>
+        <Routes />
       </main>
     </div>
   );
---
+++ src/route/index.js
@@ -0,0 +1,65 @@
+import { lazy, Suspense } from 'react'
+import { Navigate, useRoutes } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import CircularProgress from '@mui/material/CircularProgress'
+
+function CircularIndeterminate() {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CircularProgress />
+    </Box>
+  );
+}
+
+function LazyElement ({ load }) {
+  const Lazy = lazy(load)
+  return (
+    <Suspense fallback={<CircularIndeterminate />}>
+      <Lazy />
+    </Suspense>
+  )
+}
+
+const routes = [
+  {
+    index: true,
+    element: <Navigate to="/entity" replace={true} />
+  },
+  {
+    path: '/entity',
+    element: <LazyElement load={() => import('../component/class/ClassMgt')} />,
+    children: [
+      {
+        path: ':entityId',
+        children: [
+          {
+            index: true,
+            element: <LazyElement load={() => import('../component/fields/Fields')} />
+          },
+          {
+            path: ':fieldId',
+            element: <LazyElement load={() => import('../component/fields/Field')} />
+          }
+        ]
+      }
+    ]
+  },
+  {
+    path: '/contents',
+    element: <LazyElement load={() => import('../component/content/ContentMgt')} />,
+    children:[
+      {
+        index: true,
+        element: <LazyElement load={() => import('../component/content/Contents')} />
+      },
+      {
+        path: 'detail',
+        elment: <LazyElement load={() => import('../component/content/Content')} />
+      }
+    ]
+  }
+]
+
+export default function Routes () {
+  return useRoutes(routes)
+}
